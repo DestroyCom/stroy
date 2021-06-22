d0353a006d73520de4f7681c5b58f909b0257d2f
DestCom <azevedo-a@hotmail.fr>
2021-06-22 17:37:48 +0200

    Mobile css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ function Footer(){
                 <h2>STROY - A react based streaming website</h2>
                 <p>Made by <a href='https://destroykeaum.alwaysdata.net/'>Dest.Com</a></p>
             </div>
-            <div>
+            <div className='hidden'>
                 <a href='https://github.com/DestroyCom'><i class="fab fa-github fa-3x"></i></a>
                 <a href='https://twitter.com/ADSantoine'><i class="fab fa-twitter fa-3x"></i></a>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function Header({choixFilm, choixFilmUpdate, displayFilm,displayFilmUpdate}){
         <header>
             <h1 onClick={() => choixFilmUpdate('') }>STROY</h1>
             <div>
-                <i class="fas fa-search fa-lg"></i>
+                <i class="fas fa-search fa-lg" className='hidden'></i>
                 <input type='text' onChange={(e) => search(e)}/>
             </div>
             
